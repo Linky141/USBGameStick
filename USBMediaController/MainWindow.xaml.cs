efd6a7211ee5e3a62bf571041196879af012da90
Tomasz Bielas <tomaszbielas97@gmail.com>
2021-10-05 21:20:06 +0200

    change icon path

--- USBMediaController/MainWindow.xaml.cs
+++ USBMediaController/MainWindow.xaml.cs
@@ -62,7 +62,7 @@ namespace USBMediaController
             vGamepadController = vGamepadclient.CreateXbox360Controller();
             vGamepadController.Connect();
 
-            if (File.Exists(@"C:\USBGameStick\icon2.ico")) tray_main.Icon = new System.Drawing.Icon(@"C:\USBGameStick\icon2.ico");
+            if (File.Exists(@"C:\USBGameStick\iconv2.ico")) tray_main.Icon = new System.Drawing.Icon(@"C:\USBGameStick\iconv2.ico");
             else ConsoleWrite("#Error Load Icon");
 
             if (!LoadCommunicationConfig())
